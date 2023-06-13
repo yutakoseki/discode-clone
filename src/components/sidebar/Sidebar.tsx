ffd5d23c8a9176814f00aa1fb6cf1fb5c9813f2a
koseki <koseki@thinkingreed.co.jp>
2023-06-13 17:32:29 +0900

    画面UI作成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import "./Sidebar.scss"
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function Sidebar() {
   return (
@@ -17,7 +23,38 @@ function Sidebar() {
         <div className='sidebarRight'>
             <div className="sidebarTop">
                 <h3>Discode</h3>
+                <ExpandMoreIcon />
             </div>
+
+        {/* sidebarChannels */}
+        <div className="sidebarChannels">
+            <div className="sidebarChannelsHeader">
+                <div className="sidebarHeader">
+                    <ExpandMoreIcon />
+                    <h4>プログラミングチャンネル</h4>
+                </div>
+                <AddIcon className='sidebarAddIcon'/>
+            </div>
+            <div className="sidebarChannelList">
+                <SidebarChannel />
+                <SidebarChannel />
+                <SidebarChannel />
+                <SidebarChannel />
+            </div>
+            <div className="sidebarFooter">
+                <div className="sidebarAccount">
+                    <img src="./icon.png" alt="" />
+                    <h4>ShinCode</h4>
+                    <span>#8162</span>
+                </div>
+
+                <div className="sidebarVoice">
+                    <MicIcon />
+                    <HeadphonesIcon />
+                    <SettingsIcon />
+                </div>
+            </div>
+        </div>
         </div>
     </div>
   )
